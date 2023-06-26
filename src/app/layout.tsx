42cba901a471efc8dff791c098b22b9dd2df66d5
LuanCKRG <crackersage@gmail.com>
2023-06-25 22:54:16 -0400

    feat: add font and Sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
+import Sidebar from '@/components/Sidebar'
+
+import { Figtree } from 'next/font/google'
 import './globals.css'
 
+const font = Figtree({ subsets: ['latin'] })
+
 export const metadata = {
-  title: 'Spotify-Clone',
+  title: 'Spotify Clone',
   description: 'A interface clone of spotify, from LuanCK '
 }
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className={font.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
+      </body>
     </html>
   )
 }
